10046b70d6bae11ea5ce196b6fed3691316774fc
davidmilinkovic <david.milinkovic@gmail.com>
2021-09-25 13:29:05 +0200

    Dodati aktivitiji

---
+++ app/src/main/java/com/fortablydumb/whoishakatonandroid/ui/home/OmiljeniViewModel.java
@@ -0,0 +1,19 @@
+package com.fortablydumb.whoishakatonandroid.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class OmiljeniViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public OmiljeniViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
